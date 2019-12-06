7dc28498155ede180bda63266705f785ab302d1c
leonardocunha2107 <leonardocunha2107@gmail.com>
2019-12-06 10:46:36 +0100

    some stuff

--- data.py
+++ data.py
@@ -1,19 +1,34 @@
 import torch
 import numpy as np
-
-def get_edge_tensor(device=torch.device("cuda")):
-    edges=[]
+import csv
+import random
+def edge_tensors(val_cut=0.8,device=torch.device("cuda")):
+    neg_edges=[]
+    pos_edges=[]
     with open("training.txt", "r") as f:
         for line in f:
             line = line.split()
             edge=[int(line[0]),int(line[1])]
             if line[2] == '1':
-                edges.append(edge)
-            
-    edges=np.array(edges)
-    edge1,edge2=np.hstack([edges[:,0],edges[:,1]]),np.hstack([edges[:,1],edges[:,0]])
-    edges_tensor=torch.tensor(np.vstack([edge1,edge2]),device=device,dtype=torch.long)
-    return edges_tensor
+                pos_edges.append(edge)
+            else: neg_edges.append(edge)
+    random.shuffle(pos_edges)
+    random.shuffle(neg_edges)
+    pos_edges,neg_edges=np.array(pos_edges).T,np.array(neg_edges)
+    postrain_edges,negtrain_edges=pos_edges[:int(len(pos_edges)*val_cut)],neg_edges[:int(len(pos_edges)*val_cut)]
+    posval_edges,negval_edges=pos_edges[int(len(pos_edges)*val_cut):],neg_edges[int(len(pos_edges)*val_cut):]
+    val_edges=np.vstack([posval_edges,negval_edges])
+    
+    edge1,edge2=np.hstack([postrain_edges[:,0],postrainedges[:,1]]),np.hstack([postrainedges[:,1],postrainedgestrain[:,0]])
+    postrain_edges_tensor=torch.tensor(np.vstack([edge1,edge2]),device=device,dtype=torch.long)
+    
+    edge1,edge2=np.hstack([negtrain_edges[:,0],negtrainedges[:,1]]),np.hstack([negtrainedges[:,1],negtrainedgestrain[:,0]])
+    negtrain_edges_tensor=torch.tensor(np.vstack([edge1,edge2]),device=device,dtype=torch.long)
+    
+    edge1,edge2=np.hstack([val_edges[:,0],val_edges[:,1]]),np.hstack([val_edges[:,1],val_edges[:,0]])
+    valedges_tensor=torch.tensor(np.vstack([edge1,edge2]),device=device,dtype=torch.long)
+    
+    return postrain_edges_tensor,torch.cat([postrain_edges,negtrain_edges],axis=1),valedges_tensor
 
 class  TrainDS:
     def __init__(self,embed):
@@ -46,3 +61,25 @@ class TestDS:
         edge=self.edges[idx]
         x=torch.cat([self.embed[edge[0]],self.embed[edge[1]]])
         return x
+
+    
+def generate_csv(model,embeds,filename='test.csv',thr=0.5):
+    predictions = []
+    with open("testing.txt", "r") as f:
+        for l in f:
+            l = [int(x) for x in l.split()]
+            x=torch.cat([embeds[l[0]],embeds[l[1]]])
+            x=model(x)[1]
+            if x>thr:
+                predictions.append("1")
+            else: 
+                predictions.append("0")
+            
+    
+    predictions = zip(range(len(predictions)), predictions)
+    # Write the output in the format required by Kaggle
+    with open(filename,"w") as pred:
+        csv_out = csv.writer(pred)
+        csv_out.writerow(['id','predicted'])
+        for row in predictions:
+            csv_out.writerow(row)
--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import numpy as np
 from pprint import pprint
 import torch
+import torch.nn as nn
+
 
 def stats(p,y):
     """
@@ -20,40 +22,7 @@ def stats(p,y):
     pprint(metrics)
     return metrics
 
-def loader_stats(model,embedder,dataloader,threshold=0.5):
-    tp,fp,tn,fn=0,0,0,0
-    with torch.no_grad():
-        model.eval()  # set model to evaluation mode
-        
-        for x,y in dataloader:
-            x = x.to(device=device, dtype=torch.long)  
-            y = y.to(device=device, dtype=torch.long)
-
-            x=embedder(x)
-
-            scores = model(x)
-            preds = (scores[:,1]>threshold).long()
-            comp=(preds==y).long()
-
-            ##calcule standard stats
-            tp+=(comp*y).sum().data
-            fp+=((1-comp)*(1-y)).sum().data
-            tn+=((comp)*(1-y)).sum().data
-            fn+=((1-comp)*y).sum().data
-
-            
-    tp,fp,tn,fn=float(tp)+1e-5,float(fp)+1e-5,float(tn)+1e-5,float(fn)+1e-5
-    n=tp+fp+tn+fn
-    raw={'n':n,'tp':tp,'fp':fp,'tn':tn,'fn':fn}
-
-    pr,prec=tp/(tp+fn),tp/(tp+fp)
-    metrics={'acc':(tp+tn)/n,'pr':pr,'prec':prec,'nr':tn/(tn+fp),'f1':(2*prec*pr)/(prec+pr)}  
-
-    log={'raw':raw,'metrics':metrics}
-    pprint(log['metrics'])
-    return log
-
-def train_epoch(model,optim,train_dl):
+def train_epoch(model,optim,train_dl,loss_fn=nn.CrossEntropyLoss()):
     loss_avg=0
     device=torch.device("cuda")
     model.train()
@@ -72,6 +41,35 @@ def train_epoch(model,optim,train_dl):
         optim.step()
 
     
-    print('Loss %.4f' %loss)
+    print('Loss %.4f' %loss.mean())
     
     return loss_avg
+def loader_stats(model,dataloader,threshold=0.5):
+    tp,fp,tn,fn=0,0,0,0
+    with torch.no_grad():
+        model.eval()  # set model to evaluation mode
+        
+        for x,y in dataloader:
+            x = x.to(device=device, dtype=torch.float)  
+            y = y.to(device=device, dtype=torch.long)
+
+
+            scores = model(x)
+            preds = (scores[:,1]>threshold).long()
+            comp=(preds==y).long()
+
+            ##calcule standard stats
+            tp+=(comp*y).sum().data
+            fp+=((1-comp)*(1-y)).sum().data
+            tn+=((comp)*(1-y)).sum().data
+            fn+=((1-comp)*y).sum().data
+    tp,fp,tn,fn=float(tp)+1e-5,float(fp)+1e-5,float(tn)+1e-5,float(fn)+1e-5
+    n=tp+fp+tn+fn
+    raw={'n':n,'tp':tp,'fp':fp,'tn':tn,'fn':fn}
+
+    pr,prec=tp/(tp+fn),tp/(tp+fp)
+    metrics={'acc':(tp+tn)/n,'pr':pr,'prec':prec,'nr':tn/(tn+fp),'f1':(2*prec*pr)/(prec+pr)}  
+
+    log={'raw':raw,'metrics':metrics}
+    pprint(log['metrics'])
+    return log
